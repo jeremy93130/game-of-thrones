6a9be4d434ba93b3b154df47b95cceade0632bcf
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-11-04 14:58:57 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Characters from './components/Characters';
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      characters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://thronesapi.com/api/v2/Characters")
+      .then(response => response.json())
+      .then(result => {
+        this.setState({
+          characters: result.results
+        })
+      });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Game of thrones</h1>
+        <Characters />
+      </div>
+    );
+  }
 }
 
 export default App;
